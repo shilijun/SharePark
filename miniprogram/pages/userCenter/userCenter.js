1af92464fee7530e8e7dec19e00d3ab4290bf630
whuzhangzheng <303187311@qq.com>
2020-05-05 14:35:34 +0800

    Initial Commit

---
+++ miniprogram/pages/userCenter/userCenter.js
@@ -0,0 +1,77 @@
+
+//index.js
+const app = getApp()
+
+Page({
+  data: {
+    avatarUrl: '/images/user-unlogin.png',
+    nickname:null,
+    userInfo: {},
+    logged: false,
+    takeSession: false,
+    requestResult: ''
+  },
+
+  onLoad: function() {
+    if (!wx.cloud) {
+      wx.redirectTo({
+        url: '../chooseLib/chooseLib',
+      })
+      return
+    }
+      wx.getSetting({
+        success: res => {
+          if (res.authSetting['scope.userInfo']) {
+            // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+            wx.getUserInfo({
+              success: res => {
+                this.setData({
+                  logged:true,
+                  avatarUrl: res.userInfo.avatarUrl,
+                  nickname:res.userInfo.nickName,
+                  userInfo: res.userInfo
+                });
+                // 全局数据设置
+                app.globalData.userInfo = res.userInfo;
+                
+              }
+            })
+          }
+        }
+      })
+  },
+
+  onGetUserInfo: function(e) {
+    console.log(e)
+    if (!this.data.logged && e.detail.userInfo) {
+      this.setData({
+        logged: true,
+        avatarUrl: e.detail.userInfo.avatarUrl,
+        nickname:e.detail.userInfo.nickName,
+        userInfo: e.detail.userInfo
+      })
+    }
+  },
+
+  onGetOpenid: function() {
+    // 调用云函数
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {},
+      success: res => {
+        console.log('[云函数] [login] user openid: ', res.result.openid)
+        app.globalData.openid = res.result.openid
+        // wx.navigateTo({
+        //   url: '../userConsole/userConsole',
+        // })
+      },
+      fail: err => {
+        console.error('[云函数] [login] 调用失败', err)
+        wx.navigateTo({
+          url: '../deployFunctions/deployFunctions',
+        })
+      }
+    })
+  },
+
+})
